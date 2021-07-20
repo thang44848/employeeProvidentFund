a2d9e40ae957dd7f5a3223492eb521c75c861e5b
thangkumphol <thangkumphol@hotmail.com>
2021-07-20 21:23:20 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { employeeMockData } from './mock-data/employee-mock-data';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  public employeeData: any;
+  atTheMoment = moment();
+
+  ngOnInit() {
+    this.employeeData = employeeMockData;
+    this.calculateEmployeeAge();
+    this.calculateProvidentFund();
+  }
+
+  calculateEmployeeAge() {
+    this.employeeData = this.employeeData.map((employee: any) => {
+      var employeeDateOfBirth = moment(employee.birthdate , 'DDMMYYYY');
+      var employeeAge = this.atTheMoment.diff(employeeDateOfBirth, 'years');
+      return {
+        ...employee,
+        age: employeeAge
+      };
+    })
+  }
+
+  calculateProvidentFund() {
+    this.employeeData = this.employeeData.map((employee: any) => {
+      const PROBATION_PERIOD = 3;
+      const RATE_OF_GOVERNMENT_BOND = 0.02;
+      var startDateOfWork = moment(employee.startdate , 'DDMMYYYY');
+      var monthsOfWork = this.atTheMoment.diff(startDateOfWork, 'months');
+      if(employee.employeetype === 'Permanent' && monthsOfWork > PROBATION_PERIOD) {
+        var employeeContribution = (employee.salary * ( employee.pvfrate/100)) * (monthsOfWork - PROBATION_PERIOD);
+        var companyContribution = (employee.salary * (0.1)) * (monthsOfWork - PROBATION_PERIOD);
+        var governmentBond = (employeeContribution + companyContribution) * RATE_OF_GOVERNMENT_BOND;
+        var providentFundEarnInTotal = this.calculateProvidentFundEarnIfEmployeeLeaveCompanyToday(monthsOfWork, employeeContribution, companyContribution, governmentBond);
+
+        return {
+          ...employee, 
+          employeeContribution: employeeContribution,
+          companyContribution: companyContribution,
+          governmentBond: governmentBond,
+          providentFundEarnIfEmployeeLeaveToday: providentFundEarnInTotal
+        }
+        // console.log(months);
+      } else {
+        return {
+          ...employee, 
+          employeeContribution: 0,
+          companyContribution: 0,
+          governmentBond: 0,
+          providentFundEarnIfEmployeeLeaveToday: 0
+        }     
+      }
+    })
+    console.log(this.employeeData);
+  }
+
+  calculateProvidentFundEarnIfEmployeeLeaveCompanyToday(monthsOfWork: number, employeeContribution: number, companyContribution: number, governmentBond: number): number {
+    const THREE_YEARS_IN_MONTHS = 36;
+    const FIVE_YEARS_IN_MONTHS = 60;
+    var totalAmountOfProvidentFund = employeeContribution + companyContribution + governmentBond;
+    if(monthsOfWork < THREE_YEARS_IN_MONTHS) {
+      return 0;
+    } else if (monthsOfWork > THREE_YEARS_IN_MONTHS && monthsOfWork < FIVE_YEARS_IN_MONTHS) {
+      return totalAmountOfProvidentFund*0.5;
+    } else {
+      return totalAmountOfProvidentFund;
+    }
+  }
+}
---
+++ src/app/mock-data/employee-mock-data.ts
@@ -0,0 +1,122 @@
+export const employeeMockData = [
+    {
+      "employeeid": 1,
+      "firstname": "Janet",
+      "lastname": "Leverling",
+      "birthdate": "30/08/1973",
+      "startdate": "02/04/2012",
+      "employeetype": "Permanent",
+      "salary": 40000,
+      "pvfrate": 4
+    },
+    {
+      "employeeid": 2,
+      "firstname": "Nancy",
+      "lastname": "Davolio",
+      "birthdate": "08/12/1978",
+      "startdate": "01/05/2012",
+      "employeetype": "Permanent",
+      "salary": 30000,
+      "pvfrate": 5
+    },
+    {
+      "employeeid": 3,
+      "firstname": "Andrew",
+      "lastname": "Fuller",
+      "birthdate": "19/02/1962",
+      "startdate": "16/08/2012",
+      "employeetype": "Permanent",
+      "salary": 50000,
+      "pvfrate": 5
+    },
+    {
+      "employeeid": 4,
+      "firstname": "Anne",
+      "lastname": "Dodsworth",
+      "birthdate": "02/07/1969",
+      "startdate": "16/11/2012",
+      "employeetype": "Part-time",
+      "salary": 200,
+      "pvfrate": null
+    },
+    {
+      "employeeid": 5,
+      "firstname": "Michael",
+      "lastname": "Suyama",
+      "birthdate": "02/07/1963",
+      "startdate": "17/10/2013",
+      "employeetype": "Permanent",
+      "salary": 40000,
+      "pvfrate": 2
+    },
+    {
+      "employeeid": 6,
+      "firstname": "Laura",
+      "lastname": "Callahan",
+      "birthdate": "09/01/1968",
+      "startdate": "05/03/2014",
+      "employeetype": "Permanent",
+      "salary": 52000,
+      "pvfrate": 8
+    },
+    {
+      "employeeid": 7,
+      "firstname": "Fergus",
+      "lastname": "Robinson",
+      "birthdate": "18/10/1982",
+      "startdate": "02/06/2014",
+      "employeetype": "Permanent",
+      "salary": 43000,
+      "pvfrate": 10
+    },
+    {
+      "employeeid": 8,
+      "firstname": "Tom",
+      "lastname": "Brown",
+      "birthdate": "07/11/1977",
+      "startdate": "03/02/2015",
+      "employeetype": "Permanent",
+      "salary": 35000,
+      "pvfrate": 10
+    },
+    {
+      "employeeid": 9,
+      "firstname": "Margaret",
+      "lastname": "Peacock",
+      "birthdate": "11/09/1968",
+      "startdate": "01/05/2015",
+      "employeetype": "Permanent",
+      "salary": 70000,
+      "pvfrate": 10
+    },
+    {
+      "employeeid": 10,
+      "firstname": "Robert",
+      "lastname": "King",
+      "birthdate": "29/05/1970",
+      "startdate": "04/01/2016",
+      "employeetype": "Permanent",
+      "salary": 25000,
+      "pvfrate": 5
+    },
+    {
+      "employeeid": 11,
+      "firstname": "Steven",
+      "lastname": "Buchanan",
+      "birthdate": "04/03/1975",
+      "startdate": "17/10/2016",
+      "employeetype": "Part-time",
+      "salary": 150,
+      "pvfrate": null
+    },
+    {
+      "employeeid": 12,
+      "firstname": "Rebecca",
+      "lastname": "Wilson",
+      "birthdate": "20/04/1985",
+      "startdate": "01/03/2017",
+      "employeetype": "Permanent",
+      "salary": 48000,
+      "pvfrate": 4
+    }
+   ]
